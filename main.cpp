779c0e00f26a657cf2431840f6689f5bdd03f12c
Marco Bertoletti <100340284+MarcoBertoletti@users.noreply.github.com>
2022-03-29 21:58:14 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,5 +2,17 @@
 using namespace std;
 
 int main() {
+   int x=1, y=0, z, i=0, n;
+   cin >>n;
+   if(n<2){
+       cout <<"errore\n";
+   }else {
+   while(i<n){
+       z=x+y;
+       x=y;
+       y=z;
+       ++i;
+       cout <<z <<endl;
+   }}
    return 0;
 }
